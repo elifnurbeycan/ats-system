d031c3bc94680e9b81958eb32a5bf99268d635a6
Elif Nur Beycan <elif.bycn35@gmail.com>
2026-07-28 16:21:57 +0300

    chore: initialize Spring Boot ATS backend

---
+++ src/test/java/com/yasarbilgi/ats/AtsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasarbilgi.ats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
